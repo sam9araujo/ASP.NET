856ffd7b960101bbfc7dddf82dab64f385aeb82d
Samanta Araujo <samanta@sam9araujo.com>
2016-02-01 16:15:03 -0200

    insert em arquivo TXT

---
+++ HoursWork/Work-Hours/Work-Hours/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Work_Hours
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDatehorarioAtual = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtReadTXT = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.MaskedTextBox();
+            this.txtEnd = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(502, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "[+] Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Date: ";
+            // 
+            // lblDatehorarioAtual
+            // 
+            this.lblDatehorarioAtual.AutoSize = true;
+            this.lblDatehorarioAtual.Location = new System.Drawing.Point(54, 10);
+            this.lblDatehorarioAtual.Name = "lblDatehorarioAtual";
+            this.lblDatehorarioAtual.Size = new System.Drawing.Size(0, 13);
+            this.lblDatehorarioAtual.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtEnd);
+            this.groupBox1.Controls.Add(this.txtStart);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(583, 109);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Time";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "End";
+            // 
+            // txtReadTXT
+            // 
+            this.txtReadTXT.Location = new System.Drawing.Point(12, 142);
+            this.txtReadTXT.Multiline = true;
+            this.txtReadTXT.Name = "txtReadTXT";
+            this.txtReadTXT.Size = new System.Drawing.Size(583, 250);
+            this.txtReadTXT.TabIndex = 6;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(45, 19);
+            this.txtStart.Mask = "90:00";
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 20);
+            this.txtStart.TabIndex = 5;
+            this.txtStart.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(215, 19);
+            this.txtEnd.Mask = "90:00";
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 20);
+            this.txtEnd.TabIndex = 6;
+            this.txtEnd.ValidatingType = typeof(System.DateTime);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 451);
+            this.Controls.Add(this.txtReadTXT);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblDatehorarioAtual);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDatehorarioAtual;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtReadTXT;
+        private System.Windows.Forms.MaskedTextBox txtStart;
+        private System.Windows.Forms.MaskedTextBox txtEnd;
+    }
+}
+
---
+++ HoursWork/Work-Hours/Work-Hours/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Work_Hours
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string pathSRC = @"C:\02-2016.csv";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblDatehorarioAtual.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm");
+
+            StreamReader read = new StreamReader(pathSRC);
+
+            foreach (var item in read.ReadToEnd())
+            {
+                txtReadTXT.Text += item.ToString();
+            }
+
+            read.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter sWriter = new StreamWriter(pathSRC);
+            Decimal total = Convert.ToDecimal(txtStart.Text.Replace(':',' ')) + Convert.ToDecimal(txtEnd.Text.Replace(':',' '));
+            sWriter.WriteLine(DateTime.Now.ToString("dd-MM-yyyy") + ";" + txtStart.Text + ";" + txtEnd.Text + ";" + total.ToString("00:00"));
+
+            sWriter.Close();
+
+            StreamReader read = new StreamReader(pathSRC);
+
+            foreach (var item in read.ReadToEnd())
+            {
+                txtReadTXT.Text += item.ToString();
+            }
+
+            read.Close();
+            
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
